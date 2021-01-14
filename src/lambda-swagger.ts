bcabcdd3e261405419ca0d4b3a88b708eae75efa
chankung9 <59549960+chankung9@users.noreply.github.com>
2021-01-14 09:32:58 +0700

    Add swagger

---
+++ src/lambda-swagger.ts
@@ -0,0 +1,53 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { Server } from 'http'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import { createServer, proxy } from 'aws-serverless-express'
+import { eventContext } from 'aws-serverless-express/middleware'
+import * as express from 'express'
+import { ConfigService } from '@nestjs/config'
+import { AppLogger } from './shared/logger/logger.service'
+
+let cachedServer: Server
+
+const bootstrapServer = async (): Promise<Server> => {
+  const expressApp = express()
+  const adapter = new ExpressAdapter(expressApp)
+
+  const app = await NestFactory.create(AppModule, adapter, {
+    logger: false,
+  })
+  app.useLogger(new AppLogger())
+
+  app.use(eventContext())
+  app.enableCors()
+
+  const configService = app.get(ConfigService)
+  const title = configService.get('APP_TITLE')
+  const description = configService.get('APP_DESCRIPTION')
+  const version = configService.get('APP_VERSION')
+
+  const options = new DocumentBuilder()
+    .setTitle(title)
+    .setDescription(description)
+    .setVersion(version)
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('swagger', app, document)
+
+  await app.init()
+  return createServer(expressApp)
+}
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  if (!cachedServer) {
+    const server = await bootstrapServer()
+    cachedServer = server
+    return proxy(server, event, context, 'PROMISE').promise
+  } else {
+    return proxy(cachedServer, event, context, 'PROMISE').promise
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+const pjson = require('../package.json');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config'
 import { AppLogger } from './shared/logger/logger.service'
 import { RequestIdMiddleware } from './shared/middleware/request-id.middleware'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -15,6 +17,15 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService)
   const backendAppPort = configService.get('BACKEND_APP_PORT')
+
+  const options = new DocumentBuilder()
+    .setTitle(pjson.name)
+    .setDescription(pjson.description)
+    .setVersion(pjson.version)
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('swagger', app, document)
 
   await app.listen(backendAppPort)
 }
